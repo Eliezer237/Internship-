4fb85631d134729010ab6f22f034eac26439d77e
Eliezer Niyorukundo <138326048+Bigdanton@users.noreply.github.com>
2025-04-09 09:25:40 +0200

    Dachboard.css
    css added

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,46 @@
 import React from 'react';
+import "./dashboard.css"
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Welcome to the Dashboard!</h1>
-      {/* Your dashboard content goes here */}
-    </div>
-  );
-};
+    <div className='dashboard-container'>
+    
+      <div>
+      <h1 className='welcome'>Welcome To The Dashboard!</h1>
 
-export default Dashboard; 
+    </div>
+    <div className='dashboard-right'>
+      <div className='card1'>
+     <ul>
+      <li>Environmental Monitoring</li>
+      <li>Outdoor Temperature</li>
+      <li>Living Romm Temperature</li>
+      <li>Kichen Temperature</li>
+      <li>PM2.5</li>
+      <li>Humidity</li>
+      <li>Air Quality Index</li>
+     <li>Living Room Heating</li>
+     <li>Kichen &Dining Heating</li>
+      </ul> 
+      </div>
+   </div>
+
+   <div className='card2'>
+    <h1>IOT Part</h1>
+    <ul>
+      <li>Living Room Light</li>
+      <li>Kichen Light</li>
+    </ul>
+   </div>
+   
+    </div>
+
+  )
+};
+export default Dashboard;
+
+
+
+
+
+
